e1cf0f186e9c44d59f5722ea1799e19ff4d00e0f
Mr-Jones-2 <billykzaya@gmail.com>
2025-07-29 23:10:27 +0200

    Update set.js

--- set.js
+++ set.js
@@ -10,8 +10,8 @@ global.session = "https://session-id-8pge.onrender.com";
  
  
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PRINCE-MD~H4sIAAAAAAAAA5VU226jSBD9lVW/2hqDGwy2FGmhfcPEt2Bik9U+tKGBNlfTDZiM/O8r7GQSaWZnszw1fak659Sp+g7SjDJikgaMvoO8oBXmpF3yJidgBPTS90kBusDDHIMROCNRgWMXOxfb9BdWbyNniuJ5tsyKCL02ylFFHD69pP5cegDXLsjLY0zd3wTUBgjB2EkyqdlGSrXqkf15hwez3SEcjwth6T7TQj5YY/miPoBrGxHTgqbBJA9JQgocm6TZYFp8Db65Ll2lxxc8O5dwIi3ItpLMo8BT6NWXpi43HdcK1TM9zOyvwY8i2663SO4NhGUczk+1XIdOsDnRdV9sQqQJkqVOluvdRq/v8BkNUuIZHkk55c2XdV9O47M4pi/ufPqa0oMwrgnPTdYTSnu19dcDffp4uhxT0Z0HXwO+L61ibx78flNUp3CVIm8Px+t4Pdljrwnd6WJFe9A2cg8an4FvinevRP9H92YsScbLLn6N606dn3lVNtP5LArlE4GRpotitDFetkJnd/iibSxk97hvT9W5zQLnCdl8up2V5iqszdkqX4Q8yMpDJJzNOPqAj3lZ/A7lNot85NJdkOyrqcXTjbZKIm8RZKES7Fa+PMRmlCWGIlnzqhGm5XkXhIFc732nLG3bDvfKTln0lsdXfChmM8c6oUekBQ83RhFpDA+MxGsXFCSgjBeY0yy97fVhF2CvsohbEH6TF5gozI6LlHfclb4o0a4f96bPKx6tteNzaQwmOpM6ignHcLN9AF2QF5lLGCPenDKeFc2SMIYDwsDor1ulWtIFSTJOFtQDI9AfQEUdQmGgwsGf7FsdYs5wnn9LCQdd4BdZsiRgxIuSdMHtgaohXVUFSVUVcSzrIkSDoYYmYxVOkQDFaUsxuSfd0YQwjpMcjERF7g9ESZbh9e8uSMmF3/3TsoZiF/i0YNxOyzzOsPdurvdD7LpZmXKrSV3ULkgBRsLHNuGcpgFrmZUpLtyQVgS1PMDIxzEjP4QmBfHeubwND5R5bf2H0Jxp1h6BFnsb6CdtRlL/Z3lS3N4GY1rRlPwxYYykLgFdEN+fS1CCkgqV/nAoCbCN0B5cfyBvE3mEYxozMAJo0cFhWU8mRrnqq8vZTHMCDQUa+GD67ty7M0pE+97p/NrBkuFKOisny3lirg4npmVHnV6U0yQQT/pjETsPvwgCRqCuerIUDVWbHfZPzhQNN0bWN+b6+AjH7uPauiz2AcJrQhzhecD9wiJz41KxRbMx5EbfOgy+brb7xnnaBxM+NQfSo5zq24c2m0cq6pLPySLP2Q2NR9/09mwAh7DPyFqcP/U20dpwZCeBy9fttKqgLgSBI9Rx7kTC2d4ah4O4WS9odlFx5l48BRlndwhXtrroELS999Stp+O3WUrf3E5vvz4lt9H0Vqv/rOkdeGs94dr9FONt2P3LwNCPQcZTZ8B6SYnma+34GI51mZ6eJUUzZ7XDiHQxVk5tDeQ9uLY9kMeY+1mRgBFgyRGDLiiysjWykfrZbzIhTTCQdqcdY8a1j+b4Rb9J6v3WpsjyOWZhq8BM0pKydXqj5bnFMX/vNaC131w/g+s/JYcx1g4IAAA=", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
-SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
+SESSION_ID: process.env.SESSION_ID || "PRINCE-MD~H4sIAAAAAAAAA61Ua4+iSBT9L/VVZ5uHPJNOBnmI4BvR1s1+KKDEkqdUoeCk//sG7d7p7M7O9CbLJygq955z7rnnG8gLTJCLWqB+A2WFL5Ci7pW2JQIqGNaHA6pAH0SQQqCCwg23o4OvoGMmtbONdoIFQxxfKgd7riB2XdHDsCc95UkePoPXPijrIMXhTwru5ovVylqy+4DQ5KwV8GmRTXZozZZlK2jp5uilbtywE00vnsFrVxHiCuexWR5RhiqYuqhdQFx9Dv7VXnATtK3LJ4mIjRxkC5JkFr9ULAbOHSVZugo3G26SUaJ9Dv6hZmazzW0fs3Po3G49SW4Wp83eEAy5CY4TZhwL43mvTTccecAnOM5RNI5QTjFtP6/7XORlT/FHdKxZiS2ZsXHaBuG1SY/RFuZ5hIvVqpKaXbb7HHDFsjHP3G5Lw7LnPNfc+KV2TG6XOHV3geNsT3IrnzG6znz/I/BF9e6V5L/ofp60T4Hb24lb39Qu7vGs7JuV3/Pz0TBNkpVjJzJ7PMl5zAw+B38D49m6PKyDXektRmcpGSryxfUk1HjtS33CKC+1lzEOND35Dh/SuvoZylpjkVTMahm1Z6mQYrbZi6NNW/hmy69GWqYE+tJTOIZuQsaMzGCfGC9kyTiV6O4ufpw7RDvllqwwzlbQ2uYkhWKiL5/vjBLUjiOgsq99UKEYE1pBiou8O2P6AEYXD4UVondxgT/rXSaX9DZoLuvFRXnhTWRGrtS2WRRsa8ODLPKtm2fygvYM+qCsihARgiIbE1pU7RQRAmNEgPr7fU4d5QplBUUOjoAKOEniFVbkRJH9Sn67HiElsCx/yxEFfXCoimyKgEqrGvXB/b7CDQ3LYjjBGPKyLg0MSdcNURoa1sDkOIHt+GWPnmucIUJhVgKVlQRe5jhpwL32/xcYHC9pEi8opjwQLUlgBE43BJY1edPghIH1axh/9EGOGvrwcKc8z/bBAVeE+nldpgWM3g3+/hOGYVHn1GvzUO9eUAXUD8eIUpzHpCNW57AKj/iC9I4HUA8wJeivYaMKRe9c3gJML6K7B42lO+UdAXTYu0J/l0bl/ylO+rgkCJzMiJwgsKLIyir/tTvvgxx2lcCkrXD4ZYMDdPsyLGinzhvurk2EKMQpASrQ57ndc5a2Oc5JOV2ORpoZa3qsge8833fn4U5nB6cWnR72zqjRhJtuhN5OPvYw16x2DN9r1tIRbw10nBvX5x8UASpAdLORdsamtqcz03ZnY90OlVGwOoXzU57IA0aD3EEaRUZtBwt/8DTgmdEtmBJDyYz9SJ+UM/1iuzKjtcq4PnNkVno9LX7uukXogkP0sdmTx4onLhy4gizeWtubFGiVSLwcL9iTPnHS5sgRqw5qk3fSZLfn/bQ9DbbFtBG4on45X09PEyyXJTRuG8skFc/MfNuIH1t9T5X0Lc3x28bh++cBo3s4vk3jFxN9wL6nwWv/Q4W3sP2XwBp629FpGKxtzluR5Ty8xryZLcaHghJ/O5s9Eb832KUxX6xjGbx2/i9TSA9FlQEVwDyqirthqqLufDzOD8VPmukaM9beeKeQUO37bvxg3XjpcWtRFaUNyRGogF8pe17pjN5qZelRSN9XDWjd496u4PVPSG+2FpEIAAA=", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SUDO_NUMBERS: process.env.SUDO_NUMBERS || "263789306836", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
 ANTI_DELETE: process.env.ANTI_DELETE || "inboxonly", // can be set to inboxonly/allchats/true/false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
@@ -19,19 +19,19 @@ AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙", 
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by prince-mdx", // // Input Yours custom...
 MODE: process.env.MODE || "public", // Put private or public or inbox or groups
-OWNER_NUMBER: process.env.OWNER_NUMBER || "263789306836", // Only 1 owner Number Here, others Add to sudo numbers...
-OWNER_NAME: process.env.OWNER_NAME || "𝐑-𝐃V", // Input Yours custom...(Maintain font for Flow)
+OWNER_NUMBER: process.env.OWNER_NUMBER || "27781242411", // Only 1 owner Number Here, others Add to sudo numbers...
+OWNER_NAME: process.env.OWNER_NAME || "𝐌𝐑-J𝟎𝐍𝐄S", // Input Yours custom...(Maintain font for Flow)
 PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
 PACK_NAME: process.env.PACK_NAME || "💙", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
 VERSION: process.env.VERSION || "3.0.0",
-ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
+ANTILINK: process.env.ANTILINK || "delete", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
 ANTICALL: process.env.ANTICALL || "false",
-ANTIBAD: process.env.ANTIBAD || "false",
+ANTIBAD: process.env.ANTIBAD || "true",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 📵 No Calls Allowed!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐌𝐃𝐗", //  don't change 
+BOT_NAME: process.env.BOT_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃", //  don't change 
 BOT_PIC: process.env.BOT_PIC || "https://raw.githubusercontent.com/Mayelprince/url/main/menun.jpg", //  don't change 
 AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
 AUTO_BIO: process.env.AUTO_BIO || "false",
