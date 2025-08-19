68c84c64f796ccfc8ca87af78efac077e01ced22
Mr-Jones-2 <billykzaya@gmail.com>
2025-08-19 15:38:31 +0200

    Update set.js

--- set.js
+++ set.js
@@ -10,11 +10,11 @@ global.session = "https://session-id-8pge.onrender.com";
  
  
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PRINCE-MD~H4sIAAAAAAAAA61UaY+jOBD9L/5KNOEwp9TSACGBHJ2DXJ3VfnDAEIfLMZCEHuW/j0i6Z1qr2ZleaflkylbVe69e1TeQF6TEI9wA4xugjJxRhdtj1VAMDGDVUYQZ6IAQVQgYoJxQzCx+u9/m8mZpe9Ja1YYxxbq7PO80fwRnW9EWp7OgXz6BWwfQep+S4DcJey/j3fnE2BpqWhSd/K5eN5maDNyBEIb7CK9eTT3vb/cC8Z7Arc2ICCN57NADzjBD6Qg3M0TY5+BPzXJNX67BXqfXzQWqkPrDgiw5Kg95p16srig5LPxNcl6bn4NfQi6DLn3ltq51vKp7daY77NnPzs34tLS6qRI4eATlSMjjB/ySxDkOvRDnFamaT+sOJyN7Kc5f86W9ENJq6y2dsCdp+CrPNokiYP08PkWZKx7TyeeAhxe3qe3ckiBZ+ePrSdJfk0BXfAJde4ufJ7yi7pZBVg5fzY/AZ+zdK8l/0T2xPEdn22fJjl7MVFG9hTqcO2d4UZ8TrZbDZqP1/bkLzXT+OfiRlO71KE7pee3IfHxW6u4mmOz2OwyhOx80r2M/PpxfhqJ/+QkfVTX7HcqTsocTaydysns4mmIhHiMapJyr2Jo2zHtjVytMzws20YI7pvnc4iKx4KeHQLoEF2mcdVlmr/lDQkOO5SzSp4thvrEuT3dGCW68EBjCrQMYjklZMVSRIr/HYAeg8OzjgOHqri6wJmLhOyPFw4KyEFTErAxJh+NqLnN+bDkTxe3zjNjZgs2fQAdQVgS4LHHokrIqWDPBZYliXALjr3ujWs4MZ0WFhyQEBhBVVdNkASqi8LX8cjmgqkSUfslxBTogYkU2wcCoWI074P7elFRHtS1HEHtCj5c0VYJqT7dFKEMNalBuCWaPmkuS4bJCGQWGoMoQCrKuS7fO/wJDsKQ+lBVBdDRbVQRVthRbEC1bVhTJFvrCH2H83QE5vlYPE7fSS0IHRISV1SqvaVqg8N3h75coCIo6r/wmD+z2gBkwPoRxVZE8LltidY5YcCBnbLc8gBGhtMQ/uo0ZDt+5vG0wuwhbE8LhSt1KCxu02NtE/5TG+IU46eORoimKpCkCL8tQ5w3haxu//YDXZgtxhUhaAgPY08w7FrzjTMmgCeaDgenEph2b4Ced9xl5mHDHs2Lcb+QXOvKnK45Xr6cs4YV6sdwdVXKAa50eeSoKbq49/SIJMMAcbboXmeeosmNCMh8sG+663z1r82Ipzs5rxU9ml8WhUPunnVrbQclG+qt28kq6G+y79tSBFqzRIqs2iWCe3O1xRYJBz3xqq4X4TAL8sZgfSvONFDJG1nUXxYQ7st2Un5bZaR0pdLXTVMUflVZv5rEw45Eyl44LEupiZdWOuV9NitPFGhXTdMZnXO9qmcd6rL1P7317pG9bm7wNFrn/RgTfl2CO2vb9qXEP2K27+FvnQ4a3pfovi8nahCPFvgb1ZuQnx3RWTTVE6fPzPlmJLxwscD9CL/oorBZyF9xam9MUVVHBMmAAlIesICHoAFbUrV29PCp+U8w2ec+MH7xTVFbmzxH4xVRpyuPVjBXUReWhlWCtqqdV6+fGpNSvUPU+UcBsv5mUg9t35baDz3kIAAA=", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SESSION_ID: process.env.SESSION_ID || "PRINCE-MD~H4sIAAAAAAAAA5VU25KiSBT8l3rVGLmIoBEdMdwUvGHjBXRjHkoooAQKuihRnPDfN9Dpnn7Yne19K4qKPHkyT56fgBS4QjPUgNFPUFJcQ4baI2tKBEZAO0cRoqALQsggGIG1dTLi7dgoGitUl7V3DGYXTulU+NJ3ymQ/2dS8zo5meSDBC7h3QXk+Zjj4A6A40IacFq3z8uysZdmerw9DU7Xn2Jxz8ID5TVje5tZbM1grL+DeIkJMMYnNMkE5ojCboWYFMf0afXuxPBtoQfqlVXCL2pcuIubqGz886eMbEXeGqXRiLsx5P/4afUMIDq5fUp6tBsmbek3KssMyxT0sUH/saYLbk4W+4+wN03zSr3BMUGiHiDDMmi/rrmvXWd9GglogcZfkc85PPGoJxuZMGpYm1yPeu6p1O0Fif434yst5VrqnoLOaCBcX8ZtUPPJvtU/DCrm6oTmNp/QClVj2Z+Ir+j4r6f/R3bPxQDaWtMPL6i6+TnezyMijbZTPY8szinyQ1vbSKxYXwn2N/r6+9OBR9pLQPsJIE2SPrt2hrlvFvn+CkzpYXlaahKIe94k+ZGf6J5bBNDXnOjMzYaUuhct83AgGX/QH4pu0XzgucxUzkveVlG9hvxnepsEsVnFPo05s2m42mR2zS97Xx5FeRd6041Bu6Cx19eXRUYoaOwQj/t4FFMW4YhQyXJD2bih0AQzrNQooYg91wdx88yM/DdVqQ7j5cUc6ai2eZak5LSZKtY10RVEPzF6X9PICuqCkRYCqCoUWrlhBmwWqKhijCoz++tEFBF3Z07e2msh3QYRpxbbkXGYFDN9Nff8Jg6A4E7ZuSKC3B0TBiPt9jRjDJK5aGc8E0iDBNdITyCowimBWoY8GEUUhGDF6Rh+h1Yuw1d0brniJd9egC/KHHzgEIyDIA0niRE5W5BH/vfp2aUFhWX4jiIEuyB6PBiLHCzw/lGRBkpUR/729vn+wa8FCxCDOqjY0M7LuTWPNnNf6KdhOJqoZq3qsgt/dvE/FU/ZxXrCCk5CbLCYkDBZD/1pPXtM8J5b/6oivciwuD72DlU7Ml38AaY2DV68fYqlXJjKjx4hsst0WMn+1uLKTN9tXh2yPx1fSOIKie6pY7PNxZ9b3D9GOSsPBeT4LXK3TH7yN47XrX8bhUai0+KWtFqIaB+hzMdPyPTPmm32wqQPFlBp3m+x7dKYVtDLDyRDWzjZdGo2jiJZwolcNlvF+d0tTIU8KXuFfZ5dbYxJ18Rqup3bdP9SDUr085/WRl+zXnsKPUWp9aj8jjB6xJ7B17798e9Juh4u7dz8h/Foj/xJFzT28zeHS8dP9RitF31jtlOM1nU7L8spzBuOc/vLmRYIraQm43390QZlBFhU0ByMASUgLHIIuoMW5nVabRMUfl6ttG6/PvjNYMfV3AjY4RxWDeQlGvCxJkijLovB8taJFacEqeSzTVNLA/W+JIAbrTQcAAA==", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
 SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
-ANTI_DELETE: process.env.ANTI_DELETE || "inboxonly", // can be set to inboxonly/allchats/true/false
+ANTI_DELETE: process.env.ANTI_DELETE || "true", // can be set to inboxonly/allchats/true/false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
-AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "false",
 AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by prince-mdx", // // Input Yours custom...
@@ -26,7 +26,7 @@ PACK_NAME: process.env.PACK_NAME || "💙", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
 VERSION: process.env.VERSION || "3.0.0",
 ANTILINK: process.env.ANTILINK || "delete", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
-ANTICALL: process.env.ANTICALL || "false",
+ANTICALL: process.env.ANTICALL || "true",
 ANTIBAD: process.env.ANTIBAD || "true",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 📵 No Calls Allowed!_*",
@@ -40,7 +40,7 @@ CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for a
 WELCOME: process.env.WELCOME || "false",
 GOODBYE: process.env.GOODBYE || "false",    
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
-AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799", // Add Multiple Country Codes Separated by Comma...
+AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799,263,27,233,234,91,92,254", // Add Multiple Country Codes Separated by Comma...
 PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
 TIME_ZONE: process.env.TIME_ZONE || "Africa/Douala", // Enter yours else leave blank if not sure
 };
