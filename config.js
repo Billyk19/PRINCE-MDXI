2d34c5cfd185ebedfe33d0662ecb296d2edb762e
Mr-Jones-2 <billykzaya@gmail.com>
2025-12-07 21:09:58 +0200

    Update configuration values in config.js

--- config.js
+++ config.js
@@ -4,17 +4,17 @@ if (fs.existsSync('.env'))
 const path = require("path");
 
 module.exports = { 
-    SESSION_ID: process.env.SESSION_ID || 'PASTE YOUR SESSION ID HERE',
+    SESSION_ID: process.env.SESSION_ID || 'PRINCE-MD~H4sIAAAAAAAAA5VU2ZKiSBT9l3zVaDaRJaIiBhAV3FDAEif6IYUUU1aTRIUO/70Dq6qrH2Z6anhKcjn33HvOvT9AXuAKzVAD1B+gJPgKKeqWtCkRUIFeH4+IgD6IIIVABcHk7A6ZhCnj6jXVMk5K25HSyPhOZHIQ9i3LJ2h6WN/rXfECHn1Q1ocUh38ATOcraTrcZxfrPJIicZRcrzTfkGBmraNpvF76/sGdePsAsuYLeHSIEBOcx2Z5QhkiMJ2hxoGYfI1+6AhLrqh9n9sw8r5GxKmCYnSir1tno+mRic1mtDG0cpzHX6M/5cJQZ5dJEjgz5ehjxs74cE5qRsazohf3mAu339v1vHrW49EHFY5zFFkRyimmzdfrbuOrYrOiaOx652VZzBJvPz01s3mrt/r1dS3Zk9lEKIdzzvoa8ZCdN6wphnQ5YTSxGXgEIpZ1Nqita+Zy8kN+3ApIiWAy+J24Qz68kvyfumsGu/XtwU4/K4tUYQ88GfB1Pl3JB7b1qilDJOe8ixh0Sb5oGzdwSLtc8CydsrpxGmbCrXD3lUBWfLKztT03ueXyfLW+NeYnfUhr8ieW5VyJY0862eG6LAcDudktmZa1i9XFiKT1ze/VQbvAzVTRVpLI3Fp+aAfLxcXP/JM7PinV3Ux79tlub+sWHZl4Gu2znaa9PDNKUGNFQOUefUBQjCtKIMVF/tzj+gBGVxeFBNFndcHWuJ83ZJf0jtzKWyplpk9SPzjO0pF/Swd7V4IcPKOgXcytF9AHJSlCVFUomuKKFqRZoKqCMaqA+vf3PsjRnb7p1kUTuD44YlJRP6/LtIDRh6gfhzAMizqnbpOHRrdABKjs5zaiFOdx1ZWxziEJT/iKjBOkFVCPMK3QrwQRQRFQKanRr6Y1iqiruzgfWxN77IA+yJ564AiogJeG7JAdSgLHq4L0V/Xt1qHCsvyWIwr6IIfdZeBmRYIR6IP0+UpgBZYbyjwny6LwfNjtP37x7eAjRCFOK6ACw2qtAlZTc8F7nJxMJpoZa0asgc/8PnzyJsRuxAUerWYbe90zD/IoWNWHXG8X1nJwG9Ol73IzsQkN+e76L/8AAlSAt2mRMbp7Uda10lSBOd9dynC7EZtVyl1NPUkbKGee3TKOszjmpbPdZL17lF3jQy570sg5y7q9P4leNjQOy2bgyrvGWL900SJ0xSH6PdiZnw9dOZr7M2Wo0XFUyrXvXc66EGyFrQjl3f3ixLs9L48Lxpwsktganr1InBRHU7iFMPYL20qV9iKUp/MmCe4lvGHj3cHPDkrfJxd+mqtTrvs9YvQcBO8S/ZeSb7w7v7GP/m8Q75PlX7pT396UV1M6GInrO6cJo/Ejkom3sDVGaKEfvK1J3TrMtevCT8Hj8b0PyhTSY0EyoIIqO0DQB6SoO/da+bH4QyRDYy19HVtd1imsqPbZER7OUEVhVgKVk4YiJ/CSqLzdckhRTmF1AipwXhNR79zdaGXpUkg/+gto3Wft9uDxExEfSZR6BwAA',
     PREFIX: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐓𝐄𝐂𝐇",
-    OWNER_NUMBER : process.env.OWNER_NUMBER || "237682698517", 
-    SUDO_NUMBERS : process.env.SUDO_NUMBERS || "237682698517",
+    OWNER_NAME: process.env.OWNER_NAME || "𝐌.J",
+    OWNER_NUMBER : process.env.OWNER_NUMBER || "27781242411", 
+    SUDO_NUMBERS : process.env.SUDO_NUMBERS || "27781242411",
     BOT_NAME : process.env.BOT_NAME || '𝐏𝐑𝐈𝐍𝐂𝐄 𝐌𝐃𝐗',
     FOOTER : process.env.FOOTER || 'ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ ᴛᴇᴄʜ',
     CAPTION : process.env.CAPTION || '𝐏𝐑𝐈𝐍𝐂𝐄 𝐌𝐃𝐗',
     VERSION: process.env.VERSION || '4.0.0',
     BOT_PIC : process.env.BOT_PIC || 'https://raw.githubusercontent.com/Mayelprince/url/main/menun.jpg',            
-    MODE: process.env.MODE || "private",
+    MODE: process.env.MODE || "public",
     PM_PERMIT: process.env.PM_PERMIT || 'false',
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
     TIME_ZONE: process.env.TIME_ZONE || "Africa/Douala",
@@ -28,7 +28,7 @@ module.exports = {
     ANTICALL : process.env.ANTICALL || 'false', // (decline/true)/block/false
     ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
     WELCOME_MESSAGE : process.env.WELCOME_MESSAGE || 'false',
-    ANTILINK : process.env.ANTILINK || 'false', // or delete or kick or true
+    ANTILINK : process.env.ANTILINK || 'delete', // or delete or kick or true
     AUTO_LIKE_STATUS : process.env.AUTO_LIKE_STATUS || 'true',
     AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || 'true',
     STATUS_LIKE_EMOJIS : process.env.STATUS_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙",
